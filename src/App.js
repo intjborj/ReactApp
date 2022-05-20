e69bc7e5c61b4006bc8fd0540a05a9c2e756c5db
Jestoni <intjborj@gmail.com>
2022-05-20 22:36:58 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hellow world
+          Learn React
         </a>
       </header>
     </div>
